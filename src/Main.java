c1ae0183d57fcaa19b47133d24fe42f99eaf7153
Gabriele B <the.real.gabryy@gmail.com>
2025-12-26 15:36:36 +0100

    HBox and first GUI understanding

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,48 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    public static final HBox container = new HBox();
     @Override
     public void start(Stage primaryStage /*Stage is on layer 3, the deepest one*/) {
         //the root is on the graphical layer
         StackPane root = new StackPane();
         // we create a scene with that root. the scene is on layer 2
         Scene scene = new Scene(root);
-
+        scene.setRoot(root);
         primaryStage.setTitle("My first app in JavaFX");
+        root.getChildren().add(container);
 
-        //icon image
+        VBox vbox = new VBox();
+        container.getChildren().add(vbox);
+        int paddingValue = 20;
+        Insets padding = new Insets(paddingValue, paddingValue, paddingValue, paddingValue);
+        vbox.setPadding(padding);
+        vbox.prefWidthProperty().bind(primaryStage.widthProperty().divide(2));
+        vbox.setSpacing(10);
+
+        HBox box1 = new HBox();
+        HBox box2 = new HBox();
+        HBox box3 = new HBox();
+        HBox box4 = new HBox();
+        HBox box5 = new HBox();
+
+        vbox.getChildren().addAll(box1, box2, box3, box4, box5);
+
         Image img = new Image("logo-modified.png");
         primaryStage.getIcons().add(img);
-
         //fullscreen
         primaryStage.setMaximized(true);
-
         //very important command. we set the new scene, that has the new root. so we complete the 3 laers
         primaryStage.setScene(scene);
         primaryStage.show();
